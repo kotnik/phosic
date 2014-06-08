35144f13c3d916e4a4034e80ac7bd10c97ca2516
Nikola Kotur <kotnick@gmail.com>
2014-06-08 17:09:03 +0200

    app: updated celery config

--- phosic/tasks.py
+++ phosic/tasks.py
@@ -1,10 +1,6 @@
-from celery import Celery
-
 from flask_app import make_celery, db
 
-# celery = Celery('tasks')
 celery = make_celery()
-celery.config_from_object('celery_config')
 
 @celery.task()
 def calculate_statistics(x, y):
