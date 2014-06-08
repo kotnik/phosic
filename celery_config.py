3e481250a625837f716d7c370c0bb26a6ecdad01
Nikola Kotur <kotnick@gmail.com>
2014-06-08 17:01:38 +0200

    app: fixed sheduled task name

--- celery_config.py
+++ celery_config.py
@@ -3,7 +3,7 @@ from celery.schedules import crontab
 
 CELERYBEAT_SCHEDULE = {
     'every-minute': {
-        'task': 'phosic.tasks.add',
+        'task': 'phosic.tasks.calculate_statistics',
         'schedule': crontab(minute='*/30'),
         'args': (1,2),
     },
