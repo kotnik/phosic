1b489e43f28e9775c49912583104be9b54f0af07
Nikola Kotur <kotnick@gmail.com>
2014-06-11 10:38:18 +0000

    app: making production work

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask_app import app, config
+import flask_app
 from phosic import __all__
 from phosic.utils import setup_logging
 
+app = flask_app.app
+
 if __name__ == '__main__':
     setup_logging(verbose=config.get('DEBUG', False), stderr=not config.get('DEBUG', False), color=True, appname="phosic")
-    app.run(port=config.get('PORT'), debug=config.get('DEBUG', False))
+    app.run(port=config.get('PORT'), debug=app.config.get('DEBUG', False))
