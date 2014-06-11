68224a8237a74f3f2fd384cede1da3ea3a5ef761
Nikola Kotur <kotnick@gmail.com>
2014-06-11 12:39:09 +0200

    app: fixing missing config

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from phosic.utils import setup_logging
 app = flask_app.app
 
 if __name__ == '__main__':
-    setup_logging(verbose=config.get('DEBUG', False), stderr=not config.get('DEBUG', False), color=True, appname="phosic")
-    app.run(port=config.get('PORT'), debug=app.config.get('DEBUG', False))
+    setup_logging(verbose=app.config.get('DEBUG', False), stderr=not app.config.get('DEBUG', False), color=True, appname="phosic")
+    app.run(port=app.config.get('PORT'), debug=app.config.get('DEBUG', False))
